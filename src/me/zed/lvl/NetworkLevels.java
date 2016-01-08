0ca8e77aee54d93b95308ad6e2dcdf1d8b66cc53
mmxw11 <matias.korpela@hotmail.fi>
2016-01-08 21:47:01 +0200

    Cleaning
    Cleaned some stuff and make it more compact.
    + removed listener because it's not used.

--- src/me/zed/lvl/NetworkLevels.java
+++ src/me/zed/lvl/NetworkLevels.java
@@ -3,10 +3,6 @@ package me.zed.lvl;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerExpChangeEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.DisplaySlot;
@@ -15,31 +11,28 @@ import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 
-public class NetworkLevels extends JavaPlugin implements Listener {
+public class NetworkLevels extends JavaPlugin {
 
 	public void onEnable() {
-		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-		
-		for(Player online : Bukkit.getOnlinePlayers()){
-			ScoreboardManager manager = Bukkit.getScoreboardManager();
-			Scoreboard board = manager.getNewScoreboard();
 
-			Objective objective = board.registerNewObjective("expshow", "exp");
-			objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
-			objective.setDisplayName(ChatColor.GOLD + Symbols.star);
+		ScoreboardManager manager = Bukkit.getScoreboardManager();
+		Scoreboard board = manager.getNewScoreboard();
 
-			new BukkitRunnable() {
-				
-				public void run() {
-					for (Player online : Bukkit.getOnlinePlayers()) {
-						Score score = objective.getScore(online);
-						score.setScore(online.getLevel());
-					}
-					for(Player online : Bukkit.getOnlinePlayers()){
-						online.setScoreboard(board);
-					}
+		Objective objective = board.registerNewObjective("expshow", "exp");
+		objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
+		objective.setDisplayName(ChatColor.GOLD + Symbols.star);
+
+		new BukkitRunnable() {
+
+			@SuppressWarnings("deprecation")
+			public void run() {
+				for (Player online : Bukkit.getOnlinePlayers()) {
+					Score score = objective.getScore(online);
+					score.setScore(online.getLevel());
+
+					online.setScoreboard(board);
 				}
-			}.runTaskTimer(this, 0, 20);
-		}
+			}
+		}.runTaskTimer(this, 0, 20);
 	}
 }
